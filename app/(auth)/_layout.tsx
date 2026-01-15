bd7bdf028e6fa78019c872978fe7a94d6ea991ac
Dineth0 <dinethnakandala10@gmail.com>
2026-01-16 00:32:53 +0530

    add auth layout

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router"
+
+const AuthLayout = () =>{
+    return(
+        <Stack
+            screenOptions={{
+             headerShown: false,
+             animation: "slide_from_right"
+            }}>
+
+            <Stack.Screen name="login" options={{title: "Login"}}/>    
+
+        </Stack>
+    )
+}
+export default AuthLayout
