81718636dd3f2fec1bd85062ff8020d9df911935
Dineth0 <dinethnakandala10@gmail.com>
2026-01-16 12:57:47 +0530

    add handle Register

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,8 +1,40 @@
+import { useLoader } from "@/hooks/useLoader"
+import { userRegister } from "@/services/authService"
 import { useRouter } from "expo-router"
-import { Keyboard, Pressable, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native"
+import { useState } from "react"
+import { Alert, Keyboard, Pressable, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native"
 
 const Register = () =>{
     const router = useRouter()
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [conPassword, setConPassword] = useState("")
+    const {showLoader, hideLoader, isLoading} = useLoader()
+
+    const handleRegister = async () =>{
+        if (!name || !email || !password || !conPassword || isLoading) {
+            Alert.alert("Please fill all fields...!")
+            return
+        }
+        if(password !== conPassword){
+            Alert.alert("Password do not Match")
+            return
+        }
+        showLoader()
+
+        try{
+            await userRegister(name, email, password, "User")
+            Alert.alert("Account Created")
+            router.replace("/login")
+        }catch(error){
+            console.error(error)
+            Alert.alert("Field Registration")
+        }finally{
+            hideLoader()
+        }
+    }
+
     return(
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <View className="flex-1 justify-center items-center bg-gray-50 p-6">
@@ -14,29 +46,35 @@ const Register = () =>{
                         placeholder="name"
                         placeholderTextColor="#6B7280"
                         className="border bg-gray-300 p-3 mb-4 rounded-xl"
-                    
+                        value={name}
+                        onChangeText={setName}
                     />
                     <TextInput
                         placeholder="email"
                         placeholderTextColor="#6B7280"
                         className="border bg-gray-300 p-3 mb-4 rounded-xl"
-                    
+                        value={email}
+                        onChangeText={setEmail}
                     />
                     <TextInput
                         placeholder="password"
                         placeholderTextColor="#6B7280"
                         className="border bg-gray-300 p-3 mb-4 rounded-xl"
-                    
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
                     />
                     <TextInput
                         placeholder="confirm password"
                         placeholderTextColor="#6B7280"
                         className="border bg-gray-300 p-3 mb-4 rounded-xl"
-                    
+                        value={conPassword}
+                        onChangeText={setConPassword}
+                        secureTextEntry
                     />
                     <Pressable
                         className="bg-blue-600/80 px-6 py-3 rounded-2xl"
-                    
+                        onPress={handleRegister}
                     >
                         <Text className="text-white text-lg text-center">Register</Text>
                     </Pressable>
