8fd05012f7442cc4ea3f9a91a6b2b23124ac9061
Dineth0 <dinethnakandala10@gmail.com>
2026-01-16 15:23:01 +0530

    add login

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,8 +1,49 @@
+import { useLoader } from "@/hooks/useLoader"
+import { login } from "@/services/authService"
 import { useRouter } from "expo-router"
+import { useState } from "react"
 import { Keyboard, Pressable, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native"
+import { ALERT_TYPE, Toast } from "react-native-alert-notification"
 
 const Login = () =>{
     const router = useRouter()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const { showLoader, hideLoader, isLoading } = useLoader()
+
+    const handleLogin = async () =>{
+        if (!email || !password || isLoading) {
+            Toast.show({
+                type: ALERT_TYPE.WARNING,
+                title: 'Warning',
+                textBody: 'Please fill all fields...!',
+                autoClose: 3000,
+            })
+            return
+        }
+        showLoader()
+
+        try{
+            await login(email, password)
+            
+            Toast.show({
+                type: ALERT_TYPE.SUCCESS,
+                title: 'Success',
+                textBody: 'Welcome to Fans11',
+                autoClose: 3000,
+            })
+        }catch(error){
+            console.error(error)
+            Toast.show({
+                type: ALERT_TYPE.DANGER,
+                title: 'Error',
+                textBody: 'Login Faild..!',
+                autoClose: 3000,
+            })
+        }finally{
+            hideLoader()
+        }
+    }
     return(
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <View className="flex-1 justify-center items-center bg-gray-50 p-6">
@@ -14,15 +55,19 @@ const Login = () =>{
                         placeholder="email" 
                         placeholderTextColor="#6B7280"
                         className="border bg-gray-300 p-3 mb-4 rounded-xl"
+                        value={email}
+                        onChangeText={setEmail}
                     />
                     <TextInput
                         placeholder="password"
                         placeholderTextColor="#6B7280"
                         className="border bg-gray-300 p-3 mb-4 rounded-xl"
-                    
+                        value={password}
+                        onChangeText={setPassword}
                     />
                     <Pressable
                         className="bg-blue-600/80 px-6 py-3 rounded-2xl"
+                        onPress={handleLogin}
                     >
                         <Text className="text-white text-lg text-center">Login</Text>
                     </Pressable>
