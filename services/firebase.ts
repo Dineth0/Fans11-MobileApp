cd1eb6524f4a4bd6ed1a37427700c83c1f74d1d9
Dineth0 <dinethnakandala10@gmail.com>
2026-01-18 14:53:53 +0530

    add auth context

--- services/firebase.ts
+++ services/firebase.ts
@@ -2,7 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // @ts-ignore
-import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXin4VwF9IuBriqUsqlMat4MtKCHugpWY",
@@ -10,14 +10,13 @@ const firebaseConfig = {
   projectId: "fans11-97e69",
   storageBucket: "fans11-97e69.firebasestorage.app",
   messagingSenderId: "308074063251",
-  appId: "1:308074063251:web:c21af0cf4398639ee53466"
+  appId: "1:308074063251:web:c21af0cf4398639ee53466",
 };
 
 const app = initializeApp(firebaseConfig);
 
-
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
+  persistence: getReactNativePersistence(AsyncStorage),
 });
 
 export const db = getFirestore(app);
