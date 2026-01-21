fbc373ddd6d32f105e99185041491b7e00334b96
Dineth0 <dinethnakandala10@gmail.com>
2026-01-21 19:46:22 +0530

    add get all match for match service

--- services/matchService.ts
+++ services/matchService.ts
@@ -1,14 +1,25 @@
-import { addDoc, collection } from "firebase/firestore";
+import {
+    addDoc,
+    collection,
+    getDocs,
+    orderBy,
+    query,
+} from "firebase/firestore";
 import { db } from "./firebase";
 
 const matchCollect = collection(db, "players");
 
+interface Country {
+  id: string;
+  name: string;
+  flag: string;
+}
 export const addMatch = async (
   title: string,
   venue: string,
   date: string,
-  teamA: { name: string; id: string; flag: string },
-  teamB: { name: string; id: string; flag: string },
+  teamA: Country,
+  teamB: Country,
 ): Promise<string> => {
   try {
     const doc = await addDoc(matchCollect, {
@@ -23,5 +34,26 @@ export const addMatch = async (
   } catch (error) {
     console.error(error);
     throw error;
+  }
+};
+
+export const getAllMatches = async () => {
+  try {
+    const q = query(matchCollect, orderBy("createdAt", "desc"));
+    const snaphot = await getDocs(q);
+    return snaphot.docs.map((dataSet) => {
+      const data = dataSet.data();
+      return {
+        id: dataSet.id,
+        title: data.title as string,
+        venue: data.vanue as string,
+        date: data.date as string,
+        teamA: data.teamA as Country,
+        teamB: data.teamB as Country,
+      };
+    });
+  } catch (error) {
+    console.error(error);
+    return [];
   }
 };
